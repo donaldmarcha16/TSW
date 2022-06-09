4ec6855dcf8940cf400a84df8e5e77b60d52b896
dmarcha <dmarcha@YOUNGLIVING7243>
2022-06-09 10:04:44 -0400

    Add project files.

---
+++ TSW/FormTimer.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FormTimer.aspx.cs" Inherits="TSW.FormTimer" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+         <style>
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 14px 80px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 18px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.button4 {border-radius: 12px;}
+</style>
+</head>
+<body>
+    <form id="form1" runat="server">
+         <center>
+       <asp:ScriptManager ID="Scriptmanager1" runat="server">
+    </asp:ScriptManager>
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <div class="col-md-3"></div>
+	<div runat="server" class="col-md-6 well">
+		<h3 class="text-primary">Simple Stopwatch</h3>
+		<hr style="border-top:1px dotted #ccc;"/>
+		 <asp:Label ID="Label1" runat="server" style="font-size:72px" Text="00:00:00"></asp:Label>  	 
+        <br />
+            <div style="text-align:center;">                      
+            <asp:Timer ID="tm1" Interval="1000" runat="server" OnTick="tm1_Tick" />
+            <asp:Button ID="btnStart" Text="Start" BackColor="YellowGreen" CssClass="button button4" runat="server"  OnClick="Start" />
+            <asp:Button ID="btnStop" Text="Stop" BackColor="Red" CssClass="button button4" runat="server" OnClick="Stop"/>
+            <asp:Label ID="lblID" runat="server"  Text="" Visible="false"></asp:Label>
+            </div>
+          </div>	
+        </ContentTemplate>
+        <Triggers>
+            <asp:AsyncPostBackTrigger ControlID="tm1" EventName="Tick" />
+        </Triggers>
+    </asp:UpdatePanel>
+           
+              <center>
+    </form>
+</body>
+</html>
---
+++ TSW/FormTimer.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TSW
+{
+    public partial class FormTimer : System.Web.UI.Page
+    {
+        public string constr = @"Data Source=YOUNGLIVING7243\SQLEXPRESS;Initial Catalog =dbTimer;Integrated Security=True;";
+        public static Stopwatch sw;
+        DateTime DateAndTime = DateTime.Now;
+        public string DT;
+        public string ID;
+        public string IDno;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {          
+                sw = new Stopwatch();
+                lblID.Enabled = false;
+            }
+        }
+
+        protected void tm1_Tick(object sender, EventArgs e)
+        {
+            long sec = sw.Elapsed.Seconds;
+            long min = sw.Elapsed.Minutes;
+            long hour = sw.Elapsed.Hours;
+            Label1.Text = hour.ToString("00") + ":" + min.ToString("00") + ":" + sec.ToString("00");
+        }
+
+        protected void Start(object sender, EventArgs e)
+        {
+            sw = new Stopwatch();
+            sw.Start();
+            btnStop.Enabled = true;
+            btnStart.Enabled = false;
+
+            using (var con = new SqlConnection(constr))
+            {
+                string query = "insert into [tblTimer](fldCurrentTime)values(@fldCurrentTime)";
+                using (var cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    cmd.Parameters.Add("@fldCurrentTime", SqlDbType.VarChar).Value = DateAndTime;
+                    DT = DateAndTime.ToString();                
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }        
+            using (var con = new SqlConnection(constr))
+            {
+                string query = "select * from [tblTimer] where [fldCurrentTime]='" + DT + "'";
+                using (var cmd = new SqlCommand(query))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                    {
+                        sdr.Read();
+                        lblID.Text = sdr["fldID"].ToString();
+                       
+                    }
+                    con.Close();
+                }            
+            }
+        }
+
+        protected void Stop(object sender, EventArgs e)
+        {
+            sw = new Stopwatch();
+            sw.Stop();          
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            using (var con = new SqlConnection(constr))
+            {
+                string query = "update tblTimer set fldValue=@fldValue where [fldID]='" + lblID.Text + "'";
+                using (var cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    cmd.Parameters.Add("@fldValue", SqlDbType.NVarChar).Value = Label1.Text.ToString();
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            Label1.Text = "00" + ":" + "00" + ":" + "00";         
+        }
+    }
+}
